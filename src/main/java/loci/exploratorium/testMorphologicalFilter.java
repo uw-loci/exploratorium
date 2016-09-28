06d70cff2d51baf150a5578f2f94e32626ad3655
Bing <bdai6@wisc.edu>
2016-09-28 00:28:57 -0500

    Add ImageJ MorphoLibJ plugin to use morphological filters, which will be used to translate Guneet checkForBubble.m code to Hough_Circles plugin so that the updated version will be able to eliminate false positives, i.e., air bubbles, in the original Hough transform results

---
+++ src/main/java/loci/exploratorium/testMorphologicalFilter.java
@@ -0,0 +1,39 @@
+package loci.exploratorium;
+
+import ij.*;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.*;
+import java.awt.*;
+import ij.gui.*;
+import inra.ijpb.morphology.Strel;
+
+public class testMorphologicalFilter {
+	public static void main(String[] args) {
+
+		// start ImageJ
+		new ImageJ();
+
+		// open the Clown sample
+		String path = System.getProperty("user.dir") + "/images/junkfinder_25x_dic_frame1.jpg";
+		//		String path = System.getProperty("user.dir") + "/images/junkfinder_25x_dic_frame1801.jpg";
+		ImagePlus image = IJ.openImage(path);
+		image.show();
+
+		IJ.run("8-bit");
+		IJ.run("Smooth");
+		IJ.run("Find Edges");
+		//	IJ.run("Auto Threshold", "method=Default white");  // this plugin cannot be called in Java
+		IJ.run("Size...", "width=200 height=129 constrain average interpolation=Bilinear");
+		IJ.setAutoThreshold(image,"Default dark");
+		IJ.run(image, "Convert to Mask", "");
+
+		// Creates a r=2 disk structuring element
+		Strel strel = Strel.Shape.DISK.fromRadius(2);
+		// applies dilation on current image
+		ImageProcessor image1 = IJ.getImage().getProcessor();
+		ImageProcessor dilated = strel.erosion(image1);
+		// Display results
+		new ImagePlus("dilated", dilated).show();
+	}
+
+}
