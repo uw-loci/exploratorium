eda27e49868e5a8aafade68d62917e0686fc8ec0
Bing <bdai6@wisc.edu>
2016-11-04 16:31:29 -0500

    Add OpenCV-Python script for circular Hough transform to detect fish eggs

---
+++ scripts/fishTracker.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2
+import argparse
+import numpy as np
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, clone it for output, and then convert it to grayscale
+img = cv2.imread(args["image"])
+output = img.copy()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# import image as grayscale
+img = cv2.imread('images/junkfinder_25x_dic_frame1.jpg',0)
+#img = cv2.imread('images/Kip2201.jpg',0)
+#img = cv2.medianBlur(img,5)
+#cv2.imshow('original image',img)
+
+# we need to keep in mind aspect ratio so the image does
+# not look skewed or distorted -- therefore, we calculate
+# the ratio of the new image to the old image
+r = 200.0 / img.shape[1]
+dim = (200, int(img.shape[0] * r))
+ 
+# perform the actual resizing of the image and show it
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+#cv2.imshow("resized", resized)
+
+#cimg = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+#circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
+#                            param1=50,param2=30,minRadius=10,maxRadius=30)
+circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
+                            param1=20,param2=30,minRadius=30,maxRadius=50)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    print i
+    cv2.circle(resized,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(resized,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('detected fish eggs',resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
