09d522a63930eec82cebad282fa59a6c1bce080e
Bing <bdai6@wisc.edu>
2016-11-04 17:27:40 -0500

    Overlay markers of detected fish eggs on original image

--- scripts/fishTracker.py
+++ scripts/fishTracker.py
@@ -19,27 +19,29 @@ import numpy as np
 #output = img.copy()
 #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# import image as grayscale
-img = cv2.imread('../images/junkfinder_25x_dic_frame1.jpg',0)
-#img = cv2.imread('../images/Kip2201.jpg',0)
+# import image
+#img = cv2.imread('../images/junkfinder_25x_dic_frame1.jpg')  # 3 eggs
+#img = cv2.imread('../images/Kip2201.jpg')  # 1.5 eggs
+img = cv2.imread('../images/bubble3.jpg')  # air bubbles
+#output = img.copy()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grayscale
 #img = cv2.medianBlur(img,5)
 #cv2.imshow('original image',img)
 
-# we need to keep in mind aspect ratio so the image does
-# not look skewed or distorted -- therefore, we calculate
-# the ratio of the new image to the old image
-r = 200.0 / img.shape[1]
-dim = (200, int(img.shape[0] * r))
- 
-# perform the actual resizing of the image and show it
-resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+# shrink image size to make the computation faster
+# Calculate the ratio of the new image to the old image
+# make sure the aspect ratio doesn't change
+ratio = 200.0 / img.shape[1]
+dim = (200, int(img.shape[0] * ratio))
+# perform the actual resizing of the image
+resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
 #cv2.imshow("resized", resized)
 
 # Detect circles (fish eggs) in the image
 #circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
 #                            param1=50,param2=30,minRadius=10,maxRadius=30)
 circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
-                            param1=20,param2=30,minRadius=30,maxRadius=50)
+                            param1=20,param2=30,minRadius=35,maxRadius=50)
 
 # ensure at least some circles were found
 if circles is not None:
@@ -47,12 +49,17 @@ if circles is not None:
     circles = np.uint16(np.around(circles))
     
     for i in circles[0,:]:
-        # draw the outer circle
-        print i
-        cv2.circle(resized,(i[0],i[1]),i[2],(0,255,0),2)
+        # convert center and radius back to original image scale
+        (x,y,r)= np.uint16(np.around(np.dot(i,1/ratio)))
+        # draw the outer circle on original image
+        print 'Center:',x,y,'Radius:',r
+        cv2.circle(img,(x,y),r,(0,255,128),2)
         # draw the center of the circle
-        cv2.circle(resized,(i[0],i[1]),2,(0,0,255),3)
-    
-    cv2.imshow('detected fish eggs',resized)
+        cv2.circle(img,(x,y),2,(0,128,255),3)
+
+    cv2.imshow('detected fish eggs',img)
+#    cv2.imshow('detected fish eggs',np.hstack([img, output]))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+else:
+    print 'No Eggs Found'
