903ed0d480e4e735edbefbe84dbae727bf33fc63
Bing <bdai6@wisc.edu>
2016-11-04 16:46:24 -0500

    minor update

--- scripts/fishTracker.py
+++ scripts/fishTracker.py
@@ -21,7 +21,7 @@ import numpy as np
 
 # import image as grayscale
 img = cv2.imread('../images/junkfinder_25x_dic_frame1.jpg',0)
-#img = cv2.imread('images/Kip2201.jpg',0)
+#img = cv2.imread('../images/Kip2201.jpg',0)
 #img = cv2.medianBlur(img,5)
 #cv2.imshow('original image',img)
 
@@ -35,19 +35,24 @@ dim = (200, int(img.shape[0] * r))
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 #cv2.imshow("resized", resized)
 
+# Detect circles (fish eggs) in the image
 #circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
 #                            param1=50,param2=30,minRadius=10,maxRadius=30)
 circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
                             param1=20,param2=30,minRadius=30,maxRadius=50)
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    print i
-    cv2.circle(resized,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(resized,(i[0],i[1]),2,(0,0,255),3)
-
-cv2.imshow('detected fish eggs',resized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# ensure at least some circles were found
+if circles is not None:
+    # convert the (x, y) coordinates and radius of the circles to integers
+    circles = np.uint16(np.around(circles))
+    
+    for i in circles[0,:]:
+        # draw the outer circle
+        print i
+        cv2.circle(resized,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(resized,(i[0],i[1]),2,(0,0,255),3)
+    
+    cv2.imshow('detected fish eggs',resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
