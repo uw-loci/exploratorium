36d314255c47f5be9cc43ba5019d2d74e248c32a
Bing <bdai6@wisc.edu>
2016-11-07 23:05:09 -0600

    Update the code to eliminate false positives, i.e., air bubbles, from the detected Hough circles.

--- scripts/fishTracker.py
+++ scripts/fishTracker.py
@@ -10,7 +10,7 @@ import argparse
 import numpy as np
 import timeit
 
-
+"""
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "Path to the image")
@@ -20,13 +20,14 @@ args = vars(ap.parse_args())
 img = cv2.imread(args["image"])
 output = img.copy()
 #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+"""
 
 start = timeit.timeit()  # start timing
 
 # import image
 #img = cv2.imread('../images/junkfinder_25x_dic_frame1.jpg')  # 3 eggs
 #img = cv2.imread('../images/Kip2201.jpg')  # 1.5 eggs
+img = cv2.imread('../images/bubble2.jpg')  # air bubbles
 #img = cv2.imread('../images/bubble3.jpg')  # air bubbles
 #output = img.copy()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to grayscale
@@ -41,7 +42,6 @@ dim = (200, int(img.shape[0] * resizeRatio))
 # perform the actual resizing of the image
 resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
 #cv2.imshow("resized", resized)
-
 #retval,BW = cv2.threshold(resized,127,255,cv2.THRESH_BINARY)
 #cv2.imshow("binary",BW)
 
@@ -49,9 +49,11 @@ resized = cv2.resize(gray, dim, interpolation = cv2.INTER_AREA)
 def checkforBubbles(imageIn, circlesIn, threshold):
     "Eliminate false positives (air bubbles, debris, etc) from results. Basic idea is to get a binary mask around edge of circles and check if the inner boundary is dark - if dark then bubbles."
     circlesOut = np.uint16(np.zeros(circlesIn.shape))
+    
     # Create a dummy BW image of the same size as original image imgIn
     retval,BW = cv2.threshold(imageIn,127,255,cv2.THRESH_BINARY)
     # set all pixels of BW inside hough circle to 1, otherwise 0
+    BW = np.zeros(imageIn.shape, dtype="unit8")
     # Creates a r=2 disk structuring element
     # apply erosion on the BW and generate a tmp image
     # generate a ring (= BW - tmp) that only the pixels on the Hough circle edge are 1
@@ -77,32 +79,62 @@ maxRadius: Maximum size of the radius (in pixels).
 #circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
 #                            param1=50,param2=30,minRadius=10,maxRadius=30)
 circles = cv2.HoughCircles(resized,cv2.HOUGH_GRADIENT,1,50,
-                            param1=20,param2=30,minRadius=35,maxRadius=50)
+                            param1=50,param2=30,minRadius=35,maxRadius=50)
+# BGR colors
+white = (255,255,255)
+red = (0,0,255)
 
-# ensure at least some circles were found
+# Eliminate air bubbles from list of circles
+# First ensure at least some circles were found
 if circles is not None:
     # convert the (x, y) coordinates and radius of the circles to integers
-    circles = np.uint16(np.around(circles))
-    
+    circles = np.uint16(np.around(circles))  # (column #, row #, radius)
+    # initialize
+    circlesFiltered = np.uint16(np.empty(circles.shape))
+    threshold = 70
+    eggNum = 0
     for circle in circles[0,:]:
+        mask = np.zeros(resized.shape,dtype="uint8")
+        cv2.circle(mask,(circle[0],circle[1]),circle[2],white,-1)
+        #retval,mask = cv2.threshold(tmp,10,255,cv2.THRESH_BINARY)
+        se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))  # circular structure element with radius 2
+        eroded = cv2.erode(mask,se,iterations = 1)
+     #   cv2.imshow('eroded',eroded)
+        mask = cv2.bitwise_xor(mask,eroded) # only edge pixels of the circle remain unchnaged.
+        #print "mask weight:",np.sum(mask), "mask maximum:",np.max(mask)
+    #    cv2.imshow('mask', mask)
+        # keep only the pixels on the Hough circle boundary unchanged
+        ring = cv2.bitwise_and(resized,mask)
+    #    cv2.imshow('result',test)
+    #    cv2.waitKey(0)
+        averagePixelWeight = np.sum(ring)/np.sum(mask/255)
+        print "Average pixel weight:",averagePixelWeight
+        if averagePixelWeight > threshold:
+            circlesFiltered[0,eggNum,] = circle
+            eggNum +=1
+            print "Circle #", eggNum, "is a fish egg."    
+        else:
+            print "Circle #", eggNum, "is an air bubble."    
+    print "Found",eggNum, "fish eggs."
+else:
+    print 'No eggs found!'
+
+# ensure at least some fish eggs were found
+if circlesFiltered is not None:    
+    for fishEgg in circlesFiltered[0,:]:
         # convert center and radius back to original image scale
-        (x,y,r)= np.uint16(np.around(np.dot(circle,1/resizeRatio)))
+        (x,y,r)= np.uint16(np.around(np.dot(fishEgg,1/resizeRatio)))  # column #, row #, radius
         # draw the outer circle on original image
         print 'Center:',x,y,'Radius:',r
-        cv2.circle(img,(x,y),r,(0,0,255),4)
+        cv2.circle(img,(x,y),r,red,4)
         # draw the center of the circle
-        cv2.circle(img,(x,y),2,(0,128,255),5)
-
-#        cv2.circle(BW,(circle[0],circle[1]),circle[2],(255,255,255),4)
-#        cv2.circle(BW,(circle[0],circle[1]),2,(0,128,255),5)
+        cv2.circle(img,(x,y),2,red,5)
 
     cv2.imshow('detected fish eggs',img)
 #    cv2.imshow('Marks on BW image',BW)
 #    cv2.imshow('detected fish eggs',np.hstack([img, output]))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-else:
-    print 'No Eggs Found'
 
 end = timeit.timeit()
 print 'Time (seconds) to process one image:', end-start
